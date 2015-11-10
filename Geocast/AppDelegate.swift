224555e10f1e921c73c71fb588049903f846bec7
Andy Brown <andy@udacity.com>
2015-11-10 15:36:00 -0800

    Add Parse support

--- Geocast/AppDelegate.swift
+++ Geocast/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("yPrnqMrtByMdfqxBFaNDZ8wCzVZB7GU2cbjiPTg1",
+            clientKey: "oUghydO3Iu6XLcOz8v9T2gfBbPMaGM8NnUEkvCjE")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
